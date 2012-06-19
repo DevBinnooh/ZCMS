6011f955273958ddbb705685c98b8916e997ebab
devBinnooh <devBinnooh@gmail.com>
2012-06-18 23:55:04 -0700

    Added Skin to project

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,20 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    public function _initView() {
+        //init View
+        $view = new Zend_View();
+        $view->docType('XHTML1_STRICT');
+        $view->headTitle('ZCMS');
+        $view->skin = 'simpleBlue';
+        
+        //Adding View Rander
+        $viewRander = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+        $viewRander->setView($view);
+        
+        return $view;
+        
+    }
 
 }
 
