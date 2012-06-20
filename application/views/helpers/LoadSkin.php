693a4e9219980c3bb75268cc8766772819a8ce48
devBinnooh <devBinnooh@gmail.com>
2012-06-20 12:35:00 -0700

    Fixed Skin load and simplified view

--- application/views/helpers/LoadSkin.php
+++ application/views/helpers/LoadSkin.php
@@ -27,11 +27,11 @@
  *
  * @author devBinnooh <devBinnooh@gmail.com>
  */
-class Zcms_View_Helper_LoadSkin extends Zend_View_Helper_Abstract{
+class Zend_View_Helper_loadSkin extends Zend_View_Helper_Abstract{
     
     public function loadSkin($skin) {
         $skinData = new Zend_Config_Xml('./skins/'.$skin.'/skin.xml');
-        $stylesheets = $skinData->stylesheet->stylesheet->toArray();
+        $stylesheets = $skinData->stylesheets->stylesheet->toArray();
         if(isset ($stylesheets) && is_array($stylesheets)){
             foreach ($stylesheets as $stylesheet){
                 $this->view->headLink()->appendStylesheet('./skins/'.$skin.'/css/'.$stylesheet);
